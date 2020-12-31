61024cadb541a5db872b7898704e4e3c55b1fb6e
ernesto <ernestoponce27@gmail.com>
2020-12-31 20:36:47 -0300

    Save user logged on firestore

--- src/login/LoginScreen.tsx
+++ src/login/LoginScreen.tsx
@@ -3,13 +3,12 @@ import { Text } from "native-base";
 
 import {
     GoogleSignin,
-    GoogleSigninButton,
     statusCodes,
     } from '@react-native-community/google-signin';
 
 import auth from '@react-native-firebase/auth';
-import { Alert, SegmentedControlIOSComponent } from "react-native";
 import firebase from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
 
 
 export const LoginScreen = ({ navigation }) => {
@@ -38,17 +37,51 @@ export const LoginScreen = ({ navigation }) => {
     }, []);
 
     var _signIn = async () => {
+      try {
+        await GoogleSignin.hasPlayServices();
+        const userInfo = await GoogleSignin.signIn();
+    
+        console.log('userInfo', userInfo)
 
-        try {
-            await GoogleSignin.hasPlayServices();
-            const userInfo = await GoogleSignin.signIn();
-        
-            console.log(userInfo)
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(userInfo.idToken, userInfo.accessToken);
+        await auth().signInWithCredential(googleCredential);
 
-            // Create a Google credential with the token
-            const googleCredential = auth.GoogleAuthProvider.credential(userInfo.idToken, userInfo.accessToken);
-            // navigation.navigate({ name: 'Home'});
+        const user = firebase.auth().currentUser;
+        console.log('Firebase user', user);
 
+        // Check if users exists on Firestore DB
+        const usersExists = await firestore()
+                            .collection('users')
+                            .where('uid', '==', user?.uid);
+                            
+
+        usersExists.get().then(function(querySnapshot) {
+          if(querySnapshot.empty) {
+            console.log('Create new user on firestore');
+            firestore()
+            .collection('users')
+            .add({
+              name: auth().currentUser?.displayName,
+              uid: auth().currentUser?.uid
+            })
+            .then(() => {
+              console.log('User added!');
+                  navigation.reset({
+                    index: 0,
+                    routes: [
+                      {
+                        name: 'Home',
+                      },
+                    ],
+                  })
+            })
+            .catch((error) => {
+              console.log(error);
+              alert(error); 
+
+            });
+          } else {
             navigation.reset({
               index: 0,
               routes: [
@@ -57,25 +90,29 @@ export const LoginScreen = ({ navigation }) => {
                 },
               ],
             })
-
-          // Sign-in the user with the credential
-          await auth().signInWithCredential(googleCredential);
-        } catch (error) {
-          alert('Error on google login');
-          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-            // user cancelled the login flow
-            alert('Cancel');
-          } else if (error.code === statusCodes.IN_PROGRESS) {
-            alert('Signin in progress');
-            // operation (f.e. sign in) is in progress already
-          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-            alert('PLAY_SERVICES_NOT_AVAILABLE');
-            // play services not available or outdated
-          } else {
-            // some other error happened
-            console.log('ERROR ON LOGIN')
           }
+        })
+        .catch(function(error) {
+          console.log(error);
+          alert(error); 
+        });
+        // Sign-in the user with the credential
+      } catch (error) {
+        alert('Error on google login');
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+          // user cancelled the login flow
+          alert('Cancel');
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+          alert('Signin in progress');
+          // operation (f.e. sign in) is in progress already
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+          alert('PLAY_SERVICES_NOT_AVAILABLE');
+          // play services not available or outdated
+        } else {
+          // some other error happened
+          console.log('ERROR ON LOGIN')
         }
+      }
     };
 
     var signOut = async () => {
